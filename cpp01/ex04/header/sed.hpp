57f86e7933eff0a8cc21af3a7c3366c9425beae5
mtoof <mojtaba.toof@gmail.com>
2023-10-28 20:44:54 +0300

    ex04 last update

---
+++ cpp01/ex04/header/sed.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sed.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 19:31:01 by mtoof             #+#    #+#             */
+/*   Updated: 2023/10/28 19:31:56 by mtoof            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SED_HPP
+#define SED_HPP
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <string>
+
+#endif
--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -6,14 +6,11 @@
 /*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 12:31:59 by mtoof             #+#    #+#             */
-/*   Updated: 2023/10/26 14:21:08 by mtoof            ###   ########.fr       */
+/*   Updated: 2023/10/28 20:42:44 by mtoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <fstream>
-#include <cstring>
-#include <string>
+#include "sed.hpp"
 
 int	ft_error(int ac , char **av)
 {
@@ -35,13 +32,16 @@ int	ft_error(int ac , char **av)
 
 int	check_read_write_files(std::ifstream &fd_read, std::ofstream &fd_write, char *av)
 {
+	std::string des_file_name;
 	fd_read.open(av, std::ios::in);
 	if (fd_read.fail())
 	{
 		std::cout << "Couldn't open the file" << std::endl;
 		return (1);
 	}
-	fd_write.open(std::strcat(av,".replace"), std::ios::app);
+	des_file_name.append(av);
+	des_file_name.append(".replace");
+	fd_write.open(des_file_name, std::ios::out);
 	if (fd_write.fail())
 	{
 		std::cout << "Couldn't open the file" << std::endl;
@@ -51,24 +51,33 @@ int	check_read_write_files(std::ifstream &fd_read, std::ofstream &fd_write, char
 }
 int	main(int ac, char **av)
 {
-	std::ifstream	fd_read;
-	std::ofstream	fd_write;
-	std::string		read_str;
-	std::string		read_buf;
+	std::stringstream	buffer_file;
+	std::ifstream		fd_read;
+	std::ofstream		fd_write;
+	std::string			read_str;
+	std::string			read_buf;
+	size_t				last_found = 0;
+	size_t				prev_found = 0;
+	int					str1_len = 0;
 
 	
 	if (ft_error(ac, av))
 		return (1);
 	if (check_read_write_files(fd_read, fd_write, av[1]))
 		return (1);
+	str1_len = std::strlen(av[2]);
 	if (fd_read.is_open())
 	{
-		while (fd_read)
+		buffer_file << fd_read.rdbuf();
+		read_buf = buffer_file.str();
+		last_found = read_buf.find(av[2]);
+		while (last_found != std::string::npos)
 		{
-			std::getline(fd_read, read_buf);
-			read_str.append(read_buf);
-			read_str.append("\n");
+			read_str += read_buf.substr(prev_found, last_found - prev_found) + av[3];
+			prev_found = last_found + str1_len;
+			last_found = read_buf.find(av[2], prev_found);
 		}
+		read_str += read_buf.substr(prev_found);
 	}
 	fd_write << read_str;
 	fd_read.close();
