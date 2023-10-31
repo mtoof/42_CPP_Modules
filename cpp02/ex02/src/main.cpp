1706e277c998d30f75a8810edd6e8f4d60a31554
Mojtaba Toof <mtoof@c2r5p9.hive.fi>
2023-10-31 16:20:12 +0200

    CPP02 ex02

---
+++ cpp02/ex02/src/main.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 11:50:40 by mtoof             #+#    #+#             */
+/*   Updated: 2023/10/31 15:58:42 by mtoof            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/Fixed.hpp"
+
+int main( void )
+{
+	Fixed a;
+	Fixed b(11);
+	std::cout << "// Comparison Operations" << std::endl;
+	std::cout << std::boolalpha << "Less than = " << (b < a) << std::endl;
+	std::cout << std::boolalpha << "Less than equal = " << (b <= a) << std::endl;
+	std::cout << std::boolalpha << "Greater than = " << (b > a) << std::endl;
+	std::cout << std::boolalpha << "Greater than equal = " << (b >= a) << std::endl;
+	std::cout << std::boolalpha << "Not equal = " << (b != a) << std::endl;
+	std::cout << std::boolalpha << "Comparison equal = " << (b == a) << std::endl;
+	
+	std::cout  << "a = " << a << std::endl;
+	std::cout  << "b = " << b << std::endl;
+	
+	std::cout << "// Arithmetic Operations" << std::endl;
+	std::cout  << "b + a = " << (b + a) << std::endl;
+	std::cout  << "b - a = " << (b - a) << std::endl;
+	std::cout  << "b * a = " << (b * a) << std::endl;
+	std::cout  << "b / a = " << (b / 2) << std::endl;
+	
+	std::cout << "// Increment/Decrement Operations" << std::endl;
+	Fixed c(12);
+	std::cout  << "c = " << c << std::endl;
+	std::cout  << "c++ = " << c++ << std::endl;
+	std::cout  << "c = " << c << std::endl;
+	std::cout  << "++c = " << ++c << std::endl;
+	std::cout  << "c = " << c << std::endl;
+	std::cout  << "c-- = " << c-- << std::endl;
+	std::cout  << "c = " << c << std::endl;
+	std::cout  << "--c = " << --c << std::endl;
+	std::cout  << "c = " << c << std::endl;
+	
+	// Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	// std::cout << b << std::endl;
+	// std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
