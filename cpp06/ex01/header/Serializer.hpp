a8aeccbea9eafd77dd53e1d29f7aed61421b8247
mtoof <mojtaba.toof@gmail.com>
2024-02-13 13:59:08 +0200

    cpp06 ex01

---
+++ cpp06/ex01/header/Serializer.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 13:44:50 by mtoof             #+#    #+#             */
+/*   Updated: 2024/02/13 13:44:50 by mtoof            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include <iostream>
+#include <string>
+#include "Data.hpp"
+
+class Serializer
+{
+private:
+	Serializer();
+	Serializer(Serializer const &rhs);
+	Serializer &operator=(Serializer const &rhs);
+	~Serializer();
+public:
+	static uintptr_t serialize(Data* ptr);
+	static Data* deserialize(uintptr_t raw);
+};
+
+#endif
---
+++ cpp06/ex01/src/Serializer.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 13:45:02 by mtoof             #+#    #+#             */
+/*   Updated: 2024/02/13 13:45:02 by mtoof            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serializer.hpp"
+
+Serializer::Serializer(){}
+Serializer::Serializer(Serializer const &rhs){(void)rhs;}
+Serializer &Serializer::operator=(Serializer const &rhs)
+{
+	(void)rhs;
+	return (*this);
+}
+
+Serializer::~Serializer(){}
+
+uintptr_t Serializer::serialize(Data* ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* Serializer::deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data*>(raw);
+}
