a52eb8720fc6566b7759678d2c33b7c6a467fede
Mojtaba Toof <mtoof@c1r3p4.hive.fi>
2024-02-13 14:40:14 +0200

    cpp06 ex01 made a test and finished everything

--- cpp06/ex01/src/main.cpp
+++ cpp06/ex01/src/main.cpp
@@ -6,8 +6,30 @@
 /*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 13:45:10 by mtoof             #+#    #+#             */
-/*   Updated: 2024/02/13 13:45:10 by mtoof            ###   ########.fr       */
+/*   Updated: 2024/02/13 14:39:12 by mtoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
+#include "Data.hpp"
+
+int main()
+{
+	Data* data = new Data();
+	Data* deserialized_data = nullptr;
+	uintptr_t serialized_data;
+
+	data->n = 1024;
+	std::cout << "data->n = " << data->n << std::endl;
+	
+	serialized_data = Serializer::serialize(data);
+	std::cout << "serialized data = " << serialized_data << std::endl;
+	
+	deserialized_data = Serializer::deserialize(serialized_data);
+	std::cout << "\ndata address      = " << data << std::endl;
+	std::cout << "deserialized data = " << deserialized_data << std::endl;
+	std::cout << "deserialized->n = " << deserialized_data->n << std::endl;
+
+	delete data;
+	return (0);
+}
