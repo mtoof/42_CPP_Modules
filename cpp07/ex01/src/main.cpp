27ac2cdeb5b50d34cdc3b63c06decc785c84e01f
Mojtaba Toof <mtoof@c1r3p4.hive.fi>
2024-02-15 17:46:22 +0200

    formatted cpp07 ex01

--- cpp07/ex01/src/main.cpp
+++ cpp07/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 15:07:19 by mtoof             #+#    #+#             */
-/*   Updated: 2024/02/14 17:01:13 by mtoof            ###   ########.fr       */
+/*   Updated: 2024/02/15 17:41:48 by mtoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ int main()
 
 	::iter<std::string>(array, 3, ::print_array<std::string>);
 
-	int int_array[3] = {1, 2 ,3};
+	int int_array[3] = {1, 2, 3};
 
 	::iter<int>(int_array, 3, ::increment_array<int>);
 	::iter<int>(int_array, 3, ::print_array<int>);
-	
+
 	return 0;
 }
