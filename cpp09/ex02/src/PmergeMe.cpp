592893a42c9ebe41ba9f5c16c4584313da7f4927
mtoof <mojtaba.toof@gmail.com>
2024-02-29 23:54:18 +0200

    need to add the timer for the sorting process

--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:27:18 by mtoof             #+#    #+#             */
-/*   Updated: 2024/02/29 15:50:49 by mtoof            ###   ########.fr       */
+/*   Updated: 2024/02/29 23:52:19 by mtoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -166,15 +166,13 @@ void PmergeMe::insertionSort()
 {
 	size_t mid = _vec.size() / 2;
 	std::vector<int>::iterator startOfSmallNumbersArray;
-	while (!std::is_sorted(_vec.begin(), _vec.end())  && _vec.begin() + mid != _vec.end())
+	while (!std::is_sorted(_vec.begin(), _vec.end())  && _vec.begin() + mid < _vec.end())
 	{
 		startOfSmallNumbersArray = _vec.begin() + mid;
-		std::cout << "start of lowest array = " << *startOfSmallNumbersArray << std::endl;
 		std::vector<int>::iterator position;
 		position = std::lower_bound(_vec.begin(), startOfSmallNumbersArray - 1, *startOfSmallNumbersArray);
-		std::cout << "position = " << *position << std::endl;
 		_vec.insert(position, *startOfSmallNumbersArray);
-		_vec.erase(startOfSmallNumbersArray);
+		_vec.erase(startOfSmallNumbersArray + 1);
 		mid++;
 	}
 }
