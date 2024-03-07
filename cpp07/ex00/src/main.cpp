b55372e551b542b3d60567e3a70cb391c9f76ba3
Mojtaba Toof <mtoof@c1r1p4.hive.fi>
2024-03-07 14:25:14 +0200

    added extra test case to main

--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 13:56:19 by mtoof             #+#    #+#             */
-/*   Updated: 2024/02/15 17:41:59 by mtoof            ###   ########.fr       */
+/*   Updated: 2024/03/07 14:24:16 by mtoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,17 @@ int main(void)
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	float num = 1.2f;
+	float num2 = 2.3f;
+	::swap(num, num2);
+	std::cout << "num = " << num << ", num2 = " << num2 << std::endl;
+	std::cout << "min( num, num2 ) = " << ::min(num, num2) << std::endl;
+	std::cout << "max( num, num2 ) = " << ::max(num, num2) << std::endl;
+	char charVar = 'A';
+	char charVar2 = 'C';
+	::swap(charVar, charVar2);
+	std::cout << "charVar = " << charVar << ", charVar2 = " << charVar2 << std::endl;
+	std::cout << "min( charVar, charVar2 ) = " << ::min(charVar, charVar2) << std::endl;
+	std::cout << "max( charVar, charVar2 ) = " << ::max(charVar, charVar2) << std::endl;
 	return 0;
 }
